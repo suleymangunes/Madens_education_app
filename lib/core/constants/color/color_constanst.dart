430d6a0cc542e7df99abf9ae725482257c268f0f
suleyman <62201710+suleymangunes@users.noreply.github.com>
2023-04-24 18:30:50 +0300

    Workspace Created

---
+++ lib/core/constants/color/color_constanst.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ColorConstants {
+  const ColorConstants._();
+
+  static const Color broom = Color.fromRGBO(255, 237, 15, 1);
+  static const Color orangePeel = Color.fromRGBO(255, 156, 0, 1);
+  static const Color scarlet = Color.fromRGBO(255, 48, 0, 1);
+  static const Color brightRed = Color.fromRGBO(182, 0, 0, 1);
+}
